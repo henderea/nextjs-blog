2de952b195e401aff2161a743957b2ad9d6fb7fb
Eric Henderson <henderea@gmail.com>
2020-07-02 17:09:46 -0400

    switch to typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,18 @@ import utilStyles from '../styles/utils.module.scss';
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
+import { GetStaticProps } from 'next';
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
       allPostsData
     }
   };
-}
+};
 
-export default function Home({ allPostsData }) {
+export default function Home({ allPostsData }: { allPostsData: { date: string; title: string; id: string }[] }) {
   return (
     <Layout home>
       <Head>
